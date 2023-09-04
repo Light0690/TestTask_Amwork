b644a099b6ac9443952110cc62300dce88e4d489
Light0690 <106499341+Light0690@users.noreply.github.com>
2023-09-04 17:15:54 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    chunkSizeWarningLimit:3000,
     rollupOptions: {
       output: {
         manualChunks(id) {
